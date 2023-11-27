3a1461c40ca287cc50b604ddd75360e03c9ba484
Jeff Guan <jguan@lyft.com>
2023-11-26 22:35:45 -0500

    boba

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Game from './Game';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Homepage from './Homepage';
+import RateMyBoba from './ratemyboba/RateMyBoba';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+  {
+    path: "/24Game",
+    element: <Game />,
+  },
+  {
+    path: "/RateMyBoba",
+    element: <RateMyBoba />,
+  }
+]);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <Homepage /> */}
+    {/* <Game /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>,
   document.getElementById('root')
 );
