92ba3f7869fe3d1139294b0ee1fec21007627b3d
Jeff Guan <jguan@lyft.com>
2025-11-17 15:00:07 -0500

    add intro

--- src/resolutions/Resolutions.js
+++ src/resolutions/Resolutions.js
@@ -5,6 +5,7 @@ import checkIcon from './check.svg';
 import terminalIcon from './terminal.svg';
 import bagIcon from './bag.svg';
 import micIcon from './mic.svg';
+import flowers from './flowers.svg';
 
 const hoverFadeOpacity = 0.4;
 const hoverFadeDuration = 0.2;
@@ -27,8 +28,7 @@ const Resolutions = () => {
       variants={fadeIn}
       initial="hidden"
       animate="visible"
-      className="resolutionsBox"
-      transition={{ duration: 1.5 }}
+      transition={{ duration: 2.0 }}
       class="resolutionsApp"
     >
       <div className="topNav">
@@ -54,34 +54,86 @@ const Resolutions = () => {
           whileHover={{ opacity: hoverFadeOpacity }}
           transition={{ duration: hoverFadeDuration }}
           class="sideBarIcon"
-        >
-        </motion.img>
+        ></motion.img>
         <motion.img
           src={terminalIcon}
           whileHover={{ opacity: hoverFadeOpacity }}
           transition={{ duration: hoverFadeDuration }}
           class="sideBarIcon"
-        >
-        </motion.img>
+        ></motion.img>
         <motion.img
           src={bagIcon}
           whileHover={{ opacity: hoverFadeOpacity }}
           transition={{ duration: hoverFadeDuration }}
           class="sideBarIcon"
-        >
-        </motion.img>
+        ></motion.img>
         <motion.img
           src={micIcon}
           whileHover={{ opacity: hoverFadeOpacity }}
           transition={{ duration: hoverFadeDuration }}
           class="sideBarIcon"
-        >
-        </motion.img>
+        ></motion.img>
       </div>
-      <div className="appBody">
-      </div>
+      <motion.div
+        className="appBody"
+        variants={fadeIn}
+        initial="hidden"
+        animate="visible"
+        transition={{ delay: 1.0, duration: 2.0 }}
+      >
+        <Intro />
+      </motion.div>
     </motion.div>
   );
 };
 
+const Intro = () => {
+  return (
+    <div className="intro">
+      <div className="sectionTitle">habits</div>
+      <div className="section">
+        <div className="habitsImageContainer">
+          <img src={flowers} className="habitsImage"></img>
+        </div>
+        <div className="habitBox">
+          <div className="quoteBox">
+            in the eye of cacophony, time slows.
+            <br />
+            distill the moment to a single step.
+          </div>
+          <div className="questionBox">
+            <p className="questionSection">are you running too fast?</p>
+            <p className="questionSection">
+              what is the <b>single</b> most important
+              <br />
+              habit in your life?
+            </p>
+            <p className="questionSection">
+              think deeply. <br />
+              everything else fades to noise
+            </p>
+            <p className="questionSection">
+              this is not a result
+              <br />
+              this is a process
+              <br />
+              this is <b>you</b>.
+            </p>
+            <p className="questionSection">
+              who are you -<br />
+              why do you want this?
+            </p>
+          </div>
+          <motion.button
+            className="continueButton"
+            whileHover={{ opacity: hoverFadeOpacity }}
+            transition={{ duration: hoverFadeDuration }}
+          >
+            continue
+          </motion.button>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Resolutions;
